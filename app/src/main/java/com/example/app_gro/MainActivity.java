7ecbffb05b52f3a85a859fc26ebd3bde1ce19743
mjackson22-bit <jm5444859@gmail.com>
2021-10-09 16:02:26 -0600

    Backend - Navegación entre páginas
    Navegación entre Login y Registro

--- app/src/main/java/com/example/app_gro/MainActivity.java
+++ app/src/main/java/com/example/app_gro/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.app_gro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    AppCompatButton btnRegistrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnRegistrar = findViewById(R.id.btnLoginRegister);
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Registrar.class));
+            }
+        });
     }
 }
