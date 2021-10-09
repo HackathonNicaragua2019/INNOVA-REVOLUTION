f8a60d0d7cfb24c30e635c156f375e82fc8f063f
mjackson22-bit <jm5444859@gmail.com>
2021-10-09 14:36:04 -0600

    Colores
    Se han definido colores primarios y secundarios de la aplicación

--- app/src/main/java/com/example/app_gro/PantallaCarga.java
+++ app/src/main/java/com/example/app_gro/PantallaCarga.java
@@ -2,7 +2,9 @@ package com.example.app_gro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
 
 public class PantallaCarga extends AppCompatActivity {
@@ -11,5 +13,11 @@ public class PantallaCarga extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_carga);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(PantallaCarga.this, MainActivity.class));
+            }
+        }, 2500);
     }
 }
