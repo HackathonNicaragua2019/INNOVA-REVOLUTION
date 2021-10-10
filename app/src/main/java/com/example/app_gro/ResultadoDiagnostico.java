7cf2032dd4a918ffd8dda84cd896f943b92a0e36
mjackson22-bit <jm5444859@gmail.com>
2021-10-10 01:58:45 -0600

    Inicio de dasarrollo de la pantalla que mostrará el resultado del escaneo de la imagen

---
+++ app/src/main/java/com/example/app_gro/ResultadoDiagnostico.java
@@ -0,0 +1,14 @@
+package com.example.app_gro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultadoDiagnostico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado_diagnostico);
+    }
+}
