8c7e4492589a37fddc4c2edbf1bd31a1f38d7e80
mjackson22-bit <jm5444859@gmail.com>
2021-10-09 20:19:18 -0600

    Backend - Registro
    En la actividad registro se está desarrollando la implementación de
    guardar los datos de los usuarios en una base de datos

--- app/src/main/java/com/example/app_gro/Registrar.java
+++ app/src/main/java/com/example/app_gro/Registrar.java
@@ -2,13 +2,62 @@ package com.example.app_gro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Registrar extends AppCompatActivity {
-
+    EditText username, email, passwd, confirmPasswd;
+    ImageButton imgButtonGoogle;
+    Button registerUser;
+    FirebaseAuth firebaseAuth;
+    Dialog dialog;
+    protected ProgressDialog progressDialog;
+    private GoogleSignInClient signInClient;
+    private final static int RC_SIGN_IN = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+        imgButtonGoogle = findViewById(R.id.imgButtonGoogleRegister);
+        registerUser = findViewById(R.id.btnRegisterUser);
+        email = findViewById(R.id.editTextCorreoRegister);
+        username = findViewById(R.id.editTextNombreRegister);
+        passwd = findViewById(R.id.editTextPassRegister);
+        dialog = new Dialog(Registrar.this);
+        confirmPasswd = findViewById(R.id.editTextConfirmPassRegister);
+        progressDialog = new ProgressDialog(Registrar.this);
+        firebaseAuth = FirebaseAuth.getInstance();
+        registerUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String correo = email.getText().toString();
+                String pass = passwd.getText().toString();
+                String confirmPass = confirmPasswd.getText().toString();
+                if(!Patterns.EMAIL_ADDRESS.matcher(correo).matches()){
+                    email.setError("Correo Inválido");
+                    email.setFocusable(true);
+                }else if(pass.length() < 6){
+                    passwd.setError("La contraseña debe tener al menos 6 dígitos");
+                    passwd.setFocusable(true);
+                }else if(!pass.equals(confirmPass)){
+                    confirmPasswd.setError("Las contraseñas no coinciden");
+                    confirmPasswd.setFocusable(true);
+                }else{
+                    registrar(correo, pass);
+                }
+            }
+        });
+    }
+
+    private void registrar(String correo, String pass) {
     }
 }
